7442ae6e388db8e5e7966b564f9dca51143724c8
jenbroatch <54128714+jenbroatch@users.noreply.github.com>
2022-06-07 07:17:28 -0700

    Initial test commit

---
+++ scripts/VariableCreation.R
@@ -0,0 +1,17 @@
+# Variable creation for analysis of NABA/Climate data
+# Jennifer Broatch
+# jennifer.broatch@asu.edu
+# created 2022-06-07
+
+# Load additional packages
+library(tidyverse)
+
+# Load data 
+all_sites_bflies <- read_csv("data/all-sites-bflies.csv")
+all_sites_climate <- read_csv("data/all-sites-climate.csv")
+
+#View data- remove if not desired
+View(all_sites_climate)
+View(all_sites_bflies)
+
+#TO DO - Merge files
