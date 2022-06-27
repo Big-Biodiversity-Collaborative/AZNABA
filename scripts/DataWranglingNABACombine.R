dbce22de080eefb072d48863c1bce5a461dbc9e6
Brad Johnson <bjohn149@asu.edu>
2022-06-27 09:38:29 -0500

    Added post 2018 data

--- scripts/DataWranglingNABACombine.R
+++ scripts/DataWranglingNABACombine.R
@@ -10,19 +10,22 @@ library(lubridate)
 # Load data 
 matt_obs <- read_csv(file = "data/NABA-AZ-Matt.csv")
 helen_obs <- read_csv(file = "data/NABA-AZ-Helen.csv")
-#Bradly add here helenPost2018_obs <- 
+helenPost2018_obs <- read_csv("data/NABA-AZ-Helenpost2018.csv")
 
 # Drop data with no LatinName information
 matt_obs <- matt_obs %>%
   filter(!is.na(LatinName))
 helen_obs <- helen_obs %>%
   filter(!is.na(LatinName))
-
+helenPost2018_obs <- helenPost2018_obs %>% 
+  filter(!is.na(LatinName))
 
 # Convert date to Date
 matt_obs <- matt_obs %>%
   mutate(Date = as.Date(Date, format = "%m/%d/%Y"))
 helen_obs <- helen_obs %>%
+  mutate(Date = as.Date(Date, format = "%m/%d/%Y"))
+helenPost2018_obs <- helenPost2018_obs %>% 
   mutate(Date = as.Date(Date, format = "%m/%d/%Y"))
 
 # Convert Date to Year, Month, Day columns
@@ -34,14 +37,20 @@ helen_obs <- helen_obs %>%
   mutate(Year = year(Date), 
          Month = month(Date),
          Day = day(Date))
+helenPost2018_obs <- helenPost2018_obs %>% 
+  mutate(Year = year(Date),
+         Month = month(Date),
+         Day = day(Date))
 
 # Drop rows not identified to species or subspecies 
 # Rows with one word in LatinName
 helen_obs <- helen_obs %>%
   filter(str_count(string = LatinName, pattern = " ") > 0)
+helenPost2018_obs <- helenPost2018_obs %>% 
+  filter(str_count(string = LatinName, pattern = " ") > 0)
 
 # Make vector of SourceNames
-source_names <- unique(c(matt_obs$LatinName, helen_obs$LatinName))
+source_names <- unique(c(matt_obs$LatinName, helen_obs$LatinName, helenPost2018_obs$LatinName))
 
 # Make dataframe with AcceptedNames
 accepted_table <- data.frame(SourceName = source_names,
@@ -63,9 +72,12 @@ matt_obs <- matt_obs %>%
 helen_obs <- helen_obs %>%
   left_join(accepted_table, by = c("LatinName" = "SourceName")) %>%
   mutate(DataSource = "Helen")
+helenPost2018_obs <- helenPost2018_obs %>% 
+  left_join(accepted_table, by = c("LatinName" = "SourceName")) %>%
+  mutate(DataSource = "Helen")
 
 #Pull out unique latitude and longitude data
-#Combine lat and long from 2 data sets
+#Combine lat and long from 3 data sets
 lat_long <- matt_obs %>%
   select(Latitude, Longitude, Site) %>%
   bind_rows(helen_obs %>% select(Latitude, Longitude, Site)) %>%
@@ -77,13 +89,13 @@ write_csv(x=lat_long, file = "data/lat-long-site.csv")
 # Remove columns 
 az_naba <- matt_obs %>%
   select(Year, Month, Day, DataSource, Site, AcceptedName, ButterflyCount, HoursObserved) %>%
-  bind_rows(helen_obs %>% select(Year, Month, Day, DataSource, Site, 
-                                 AcceptedName, ButterflyCount))
+  bind_rows(helen_obs %>% select(Year, Month, Day, DataSource, Site, AcceptedName, ButterflyCount)) %>% 
+  bind_rows(helenPost2018_obs %>% select(Year, Month, Day, DataSource, Site, AcceptedName, ButterflyCount))
   
 # Write to file
 write_csv(x = az_naba, file = "data/all-sites-bflies.csv")
 
-
+view(az_naba)
 
 
 
