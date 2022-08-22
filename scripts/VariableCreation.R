1d86309e9213ef0bbd659c71937b701185493f43
Brad Johnson <bjohn149@asu.edu>
2022-08-22 12:30:37 -0700

    Create data files merging total butterlfy count with temperature data

--- scripts/VariableCreation.R
+++ scripts/VariableCreation.R
@@ -46,3 +46,35 @@ distinct_bflies <- count_bflies %>%
   distinct() 
 
 View(distinct_bflies)
+
+
+#Import packages and data
+library(dplyr)
+all_sites_climate <- read_csv(data/all-sites-climate.csv)
+NABA_Rowe <- read_csv(data/NABA_Rowe.csv)
+
+#Creating a data frame with total butterfly count for each site visit in the Helen data
+NABA_Rowe_Total_Count <- NABA_Rowe %>% 
+  select(Year, Month, Day, Site, ButterflyCount) %>% 
+  group_by(Year, Month, Day, Site) %>% 
+  summarize(total_butterly_count = sum(ButterflyCount)) 
+
+#Changing Site names in all-cites-climate.csv to match with NABA Data 
+all_sites_climate$site[all_sites_climate$site=="PortalAZ"]<-"Portal"
+all_sites_climate$site[all_sites_climate$site=="GrandCanyonNorthRim"]<-"Grand Canyon North Rim"
+all_sites_climate$site[all_sites_climate$site=="PatagoniaAZ"]<-"Patagonia"
+all_sites_climate$site[all_sites_climate$site=="SabinoCanyonAZ"]<-"Sabino Canyon"
+all_sites_climate$site[all_sites_climate$site=="SantaRitaMountains"]<-"Santa Rita Mountains"
+all_sites_climate$site[all_sites_climate$site=="RamseyCanyonAZ"]<-"Ramsey Canyon"
+all_sites_climate$site[all_sites_climate$site=="McDowellSonoranPreserve"]<-"McDowell Sonoran Preserve"
+
+climatelagBJ <- all_sites_climate %>% 
+  group_by(site) %>% 
+  dplyr::mutate(tmean_previous = dplyr::lag(tmean, n = 1, default = NA))
+  
+
+#Adding current month temperature and previous monthly temperature
+Total_Count_Temp <- left_join(NABA_Rowe_Total_Count, climatelagBJ, by=c("Year"="year", "Month"="month", "Site"="site"))
+
+Count_Tmean <- Total_Count_Temp %>% 
+  select(Year, Month, Day, Site, total_butterly_count, tmean, tmean_previous)
