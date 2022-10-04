a2ad546305d0b5403dbaaee55ff64c9ce697e7ca
Brad Johnson <bjohn149@asu.edu>
2022-10-04 12:42:05 -0700

    Adding party variables trying to fix bugs

--- Variable_Combining.R
+++ Variable_Combining.R
@@ -9,6 +9,7 @@ winter <- read_csv(file = "data/winter_all.csv")
 monsoon <- read_csv(file = "data/monsoon_all.csv")
 butterfly <- read_csv(file = "data/Final_butterfly2.csv") 
 baseline <- read_csv(file= "data/Butterfly_summary.csv")
+party_values <- read_csv(file= "data/AZNABA_lat_long.csv")
 
 #joining the monsoon and winter data
 Seasonal <- left_join(winter, monsoon, by=c("Site", "year"))
@@ -44,41 +45,32 @@ sampling_events<- subset(sampling_events, select = -c(Group_Thirty, Group_Twenty
 write_csv(x = sampling_events, 
           file = "data/sampling_events.csv")
 
+
 #renaming party variables
 party_values <- party_values %>% 
   rename(Total_Parties = '#Parties',
          Total_Observers = '#Observers'
   )
-
-
-#getting the party variables
-party_values <- party_values %>% 
-  select(Year, Month, Day, Site, PartyHours, Total_Parties, Total_Observers, TotalDistanceMi) %>% 
-  group_by(Year, Month, Day, Site )
-
-#removing duplicates
-party_values <- party_values %>% 
-  distinct()
-
-#joining the party variables to sampling events
-party_butterfly <- left_join(sampling_events, party_values, by=c("year"="Year", "month"="Month", "Site"="Site"))
-
+#Removing unneeded rows
 party_values <- subset(party_values, select = -c(NABAEnglishName, ButterflyCount, Longitude, Latitude))
 
+#Trying to group sampling events
 party_values <- party_values %>% 
   select(Year, Month, Day, Site, PartyHours, Total_Parties, Total_Observers, TotalDistanceMi) %>% 
   group_by(Year, Month, Day, Site,Total_Observers, TotalDistanceMi)
 
+#removing duplicates (however we have 271 values when we should have only 219)
 party_values <- party_values %>% 
   distinct(Year, Month, Day, Site, Total_Parties, PartyHours)
 
-party_values <- party_values[!(duplicated(party_values[c("Year", "Month", "Day", "Site", "PartyHours", "Total_Parties")]) 
-                               | duplicated(party_values[c("Year", "Month", "Day", "Site", "PartyHours", "Total_Parties")],
-                                            fromLast = TRUE)), ]
-
+#Removing helen/matt duplicates where distance and observers were added
 party_values<- party_values[!duplicated(party_values[c("Year", "Month", "Day", "Site", "PartyHours", "Total_Parties")],
                                         fromLast = TRUE),]
 
+#Removing duplicates in helen's dadta where I assume a copying error occurred
 party_values<- party_values[!duplicated(party_values[c("Year", "Site", "PartyHours", "Total_Parties")],
                                         fromLast = TRUE),]
 
+
+#joining the party variables to sampling events
+party_butterfly <- left_join(sampling_events, party_values, by=c("year"="Year", "month"="Month", "Site"="Site"))
