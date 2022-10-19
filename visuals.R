a2023b9071a3bb27d54eabf2a24124b07c630ed8
Brad Johnson <bjohn149@asu.edu>
2022-10-18 21:09:10 -0700

    created 4 desired graphs

--- visuals.R
+++ visuals.R
@@ -4,6 +4,7 @@ library(lubridate)
 library(dplyr)
 library(ggplot2)
 library(ggbreak)
+library(zoo)
 
 #Creating a new daily weather file 
 daily_weather <- NULL
@@ -39,6 +40,9 @@ monthly_tmean<- daily_weather %>%
   group_by(Site, year, month) %>% 
   summarize(Tmonth = mean(tmean))
 
+#creating a year-month date column
+monthly_tmean$Date <- as.yearmon(paste(monthly_tmean$year, monthly_tmean$month), "%Y %m")
+
 #grouping by yearly precip
 yearly_precip<- daily_weather %>% 
   group_by(Site, year) %>% 
@@ -59,8 +63,36 @@ p <- ggplot(bfly_summary, aes(x = date, y = total_butterly_count, color = Site))
   geom_line() +
   scale_x_date(date_labels = "%Y-%m")
 
-BflyCount <- p + scale_y_break(breaks = c(20000, 132000)) + scale_y_continuous(labels = scales::comma)
-BflyCount
+BflyAbundance <- p + scale_y_break(breaks = c(20000, 132000)) +
+  scale_y_continuous(labels = scales::comma) +
+  xlab("Date") + ylab("Butterfly Count") + ggtitle("Total Butterfly Counts for Each Sampling Event")
+
+BflyAbundance
+
+#creating graph for date and butterfly richness for each site
+r <- ggplot(bfly_summary, aes(x = date, y = Unique_butterflies, color = Site)) +
+  geom_line() +
+  scale_x_date(date_labels = "%Y-%m")
+
+BflyRichness <- r + xlab("Date") + ylab("Unique Butterfly Species") + 
+  ggtitle("Total Butterfly Species for Each Sampling Event")
+
+BflyRichness
+
+#creating graph for month and monthly site temperature
+t <- ggplot(monthly_tmean, aes(x = Date, y = Tmonth, color = Site)) + geom_line()
+BflyTemp <- t + xlab("Date") + ylab("Monthly Temperature") +
+  ggtitle("Monthly Temperature for Each Sampling Site")
+
+BflyTemp
+
+#creating a graph for yearly precip for each site
+p <- ggplot(yearly_precip, aes(x = year, y = Pyear, color = Site)) + geom_line()
+BflyPrecip <- p + xlab("Year") + ylab("Yearly Precipitation") +
+  ggtitle("Yearly Precipitation for Each Sampling Site")
+
+BflyPrecip
+
 #viewing weird counts
 santa_check <- Az_naba_all %>% filter((Site == "SantaRitaMountains") & (Year == 2021))
 
