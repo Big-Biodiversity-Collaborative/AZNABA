011fd119bcf82612b7c6701533d6f9e17c557408
Brad Johnson <bjohn149@asu.edu>
2022-11-22 11:26:10 -0700

    created df's and tried to start basic modeling

--- df cleaning.R
+++ df cleaning.R
@@ -14,6 +14,9 @@ bfly_analysis <- read_csv(file = "data/Butterfly_Analysis.csv")
 bfly_analysis <- subset(bfly_analysis, select = -c(ButterflyCount))
 names(bfly_analysis)[names(bfly_analysis) == "total_butterly_count"] <- "total_butterfly_count"
 
+#Rewriting the df
+write_csv(x = bfly_analysis, 
+          file = "data/Butterfly_Analysis.csv")
 
 #Creating the fall DF
 bfly_fall <- bfly_analysis %>% 
@@ -42,3 +45,25 @@ write_csv(x = bfly_spring,
           file = "data/Spring_Analysis.csv")
 write_csv(x = bfly_fall, 
           file = "data/Fall_Analysis.csv")
+
+
+
+
+
+#Attempting to set up a basic model 
+model = lmer(total_butterfly_count ~ year + (1|Site),
+             data=bfly_spring,
+             REML = TRUE)
+summary(model)
+
+model2 = lmer(total_butterfly_count ~ year + (1|Site),
+             data=bfly_fall,
+             REML = TRUE)
+summary(model2)
+
+
+
+
+
+
+
