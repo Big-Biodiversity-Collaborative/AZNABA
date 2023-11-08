185f1b06d1d73900e4bd43d9e85fdf50b419e517
Maxine Cruz <terese.cruz@gmail.com>
2023-11-08 10:10:25 -0700

    Modify df cleaning 3 -- reformat, rename, and move to data_wrangling folder

--- scripts/data_wrangling/separate_fall_spring.R
+++ scripts/data_wrangling/separate_fall_spring.R
@@ -1,4 +1,12 @@
-#reading in necessary packages
+# ABOUT ---
+
+# Generates the following files:
+  # butterfly_analysis_spring.csv
+  # butterfly_analysis_fall.csv
+
+
+
+# LOAD LIBRARIES ---
 library(tidyverse)
 library(lubridate)
 library(dplyr)
@@ -8,48 +16,55 @@ library(glme)
 library(DescTools)
 library(nlme)
 
-#Reading in the DF
-bfly_analysis <- read_csv(file = "data/Butterfly_Analysis.csv")
 
-#Removing butterfly count and renaming butterly to butterfly
+
+# LOAD DATA ---
+bfly_analysis <- read_csv(file = "data/butterfly_analysis.csv")
+
+
+
+# SEPARATE DATA INTO FALL AND SPRING DATA ---
+
+# Removing butterfly count and renaming butterly to butterfly
 bfly_analysis <- subset(bfly_analysis, select = -c(ButterflyCount))
 names(bfly_analysis)[names(bfly_analysis) == "total_butterly_count"] <- "total_butterfly_count"
 
-#Rewriting the df
+# Rewriting the df
 write_csv(x = bfly_analysis, 
-          file = "data/Butterfly_Analysis.csv")
+          file = "data/butterfly_analysis.csv")
 
-#Creating the fall DF
+# Creating the fall DF
 bfly_fall <- bfly_analysis %>% 
   select(year:TotalDistanceMi) %>% 
   filter(month > 6)
 
-#Removing second sampling of santarita mountains
+# Removing second sampling of santarita mountains
 bfly_fall <- bfly_fall[!(bfly_fall$Site == 'SantaRitaMountains' & bfly_fall$month == 9),]
 bfly_fall <- bfly_fall[!(bfly_fall$Site == 'SantaRitaMountains' & bfly_fall$month == 10),]
 
-#removing pre 7/15 sampling
+# Removing pre 7/15 sampling
 bfly_fall <- bfly_fall[!(bfly_fall$Site == 'GrandCanyonNorthRim' & bfly_fall$day == 5),]
 bfly_fall <- bfly_fall[!(bfly_fall$Site == 'SycamoreCreekAZ' & bfly_fall$day == 7),]
 
-#Creating the spring data observations
+# Creating the spring data observations
 bfly_spring <- bfly_analysis %>% 
   select(year:TotalDistanceMi) %>% 
   filter(month < 7)
 
-#Removing post 6/15 sampling  
+# Removing post 6/15 sampling  
 bfly_spring <- bfly_spring[!(bfly_spring$Site == 'RamseyCanyonAZ' & bfly_spring$month == 6),]
 bfly_spring <- bfly_spring[!(bfly_spring$Site == 'AtascosaHighlandsAZ' & bfly_spring$month == 6),]
 
-#Writing sring/fall df's to files
+# Save files
 write_csv(x = bfly_spring, 
-          file = "data/Spring_Analysis.csv")
+          file = "data/butterfly_analysis_spring.csv")
+
 write_csv(x = bfly_fall, 
-          file = "data/Fall_Analysis.csv")
+          file = "data/butterfly_analysis_fall.csv")
 
 
 
-
+# GENERATE MODELS ---
 
 #Attempting to set up a basic model 
 model = lmer(total_butterfly_count ~ year + (1|Site),
