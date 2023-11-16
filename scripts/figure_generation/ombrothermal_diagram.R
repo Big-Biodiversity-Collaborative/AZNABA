24be3136be2a14221228c5e60a84341b9d5c471e
Maxine Cruz <terese.cruz@gmail.com>
2023-11-16 16:17:41 -0700

    Rename script name, because I think it is "ombrothermal" since there were more results on the internet for that

---
+++ scripts/figure_generation/ombrothermal_diagram.R
@@ -0,0 +1,31 @@
+# Maxine Cruz
+# tmcruz@arizona.edu
+# Created: 16 November 2023
+# Last modified: 16 November 2023
+
+
+
+
+# ----- ABOUT THE SCRIPT -----
+
+# Generates umbrothermal plot (ombrothermic diagram?)
+  # Temperature and precipitation on y-axis
+  # Months on x-axis
+
+
+
+
+# ----- LOAD LIBRARIES -----
+
+library(tidyverse)
+
+
+
+
+# ----- LOAD DATA -----
+
+
+
+
+
+
